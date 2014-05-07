acc10771732782539fcbd2beafd902431244e85a
Victor <Victor@VMJDevPC>
2014-05-07 13:00:52 +0300

    Improve implementation with No Additional Variable string reverse. Add tests for the implementation.

--- BubbleListHashTreePrj/src/com/reverse/str/ReverseStrinImpl.java
+++ BubbleListHashTreePrj/src/com/reverse/str/ReverseStrinImpl.java
@@ -1,23 +1,48 @@
 package com.reverse.str;
 
+/**
+ * Test task.
+ * 
+ * @author Victor
+ * 
+ */
 public class ReverseStrinImpl {
 
-	public static String reverseString(String str) {
-		String result = null;
-		if (str != null) {
-			char[] array = str.toCharArray();
-			int i = 0;
-			int j = array.length-1;
-			while(i < j) {
-				char tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
-				i++;
-				j--;
-			}
-			result = new String(array);
-		}
-		System.out.println(result);
-		return result;
-	}
+    public static String reverseString(String str) {
+        String result = null;
+        if (str != null) {
+            char[] array = str.toCharArray();
+            int i = 0;
+            int j = array.length - 1;
+            while (i < j) {
+                char tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+                i++;
+                j--;
+            }
+            result = new String(array);
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    public static String reverseStringNoAdditionalVar(String str) {
+        String result = null;
+        if (str != null) {
+            char[] array = str.toCharArray();
+            int i = 0;
+            int j = array.length - 1;
+            while (i < j) {
+                array[i] = (char) (array[i] ^ array[j]); // i -> i + j
+                array[j] = (char) (array[i] ^ array[j]); // j -> i
+                array[i] = (char) (array[i] ^ array[j]); // i -> j
+                i++;
+                j--;
+            }
+            result = new String(array);
+        }
+        System.out.println(result);
+        return result;
+    }
 }
--- BubbleListHashTreePrj/src/com/reverse/str/ReverseStringTest.java
+++ BubbleListHashTreePrj/src/com/reverse/str/ReverseStringTest.java
@@ -1,20 +1,34 @@
 package com.reverse.str;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class ReverseStringTest {
 
-	@Test
-	public void testReverseStringPositive() {
-		assertTrue("cba".equals(ReverseStrinImpl.reverseString("abc")));;
-		assertTrue("12345".equals(ReverseStrinImpl.reverseString("54321")));;
-		assertTrue("tset".equals(ReverseStrinImpl.reverseString("test")));;
-	}
+    @Test
+    public void testReverseStringPositive() {
+        assertTrue("cba".equals(ReverseStrinImpl.reverseString("abc")));
+        assertTrue("12345".equals(ReverseStrinImpl.reverseString("54321")));
+        assertTrue("tset".equals(ReverseStrinImpl.reverseString("test")));
+    }
 
-	@Test
-	public void testReverseStringNegative() {
-		assertFalse("test".equals(ReverseStrinImpl.reverseString("test")));;
-	}
+    @Test
+    public void testReverseStringNegative() {
+        assertFalse("test".equals(ReverseStrinImpl.reverseString("test")));
+        ;
+    }
 
+    @Test
+    public void testReverseStringNoAdditionalVarPositive() {
+        assertTrue("cba".equals(ReverseStrinImpl.reverseStringNoAdditionalVar("abc")));
+        assertTrue("12345".equals(ReverseStrinImpl.reverseStringNoAdditionalVar("54321")));
+        assertTrue("tset".equals(ReverseStrinImpl.reverseStringNoAdditionalVar("test")));
+    }
+
+    @Test
+    public void testReverseStringNoAdditionalVarNegative() {
+        assertFalse("test".equals(ReverseStrinImpl.reverseStringNoAdditionalVar("test")));
+    }
 }
