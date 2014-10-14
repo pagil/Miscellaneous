c8c7a0a4e74ecc2c37f8edc2d107815bae5df190
truemate <patron_gilza@tut.by>
2014-10-15 00:43:13 +1100

    Crawl for all sites on the page and for all reviews on the review page.

--- WebCrawl/src/main/java/web/page/crawl/App.java
+++ WebCrawl/src/main/java/web/page/crawl/App.java
@@ -7,7 +7,7 @@ import org.jsoup.select.Elements;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String url = "http://www.glassdoor.com.au/Reviews/Telstra-Reviews-E6563.htm";
+        String url = "http://www.glassdoor.com.au/Reviews/company-reviews.htm";
         Document document = Jsoup.connect(url).userAgent("Mozilla").get();
 
         // String question = document.select(".row pros").text();
@@ -19,9 +19,26 @@ public class App {
         // System.out.println(linkText);
         // }
 
-        Elements answerers = document.select(".pros");
-        for (Element answerer : answerers) {
-            System.out.println("PROS: " + answerer.text());
+        Elements companies = document.select("a.item.org.emphasizedLink");
+        for (Element company : companies) {
+            String companyURL = company.absUrl("href");
+            String companyName = company.text();
+            Document companyPage = Jsoup.connect(companyURL).userAgent("Mozilla").get();
+            Elements reviews = companyPage.select("a.eiCell.cell.reviews");
+            String companyReviewURL = reviews.get(0).absUrl("href");
+            Document companyReviewPage = Jsoup.connect(companyReviewURL).userAgent("Mozilla").get();
+            Elements positions = companyReviewPage.select("span.authorJobTitle.cell.middle.padHorzSm");
+            Elements pros = companyReviewPage.select("p.pros.noMargVert.notranslate");
+            Elements cons = companyReviewPage.select("p.cons.noMargVert.notranslate");
+            int max = positions.size();
+            // max = max < pros.size() ? pros.size() : max;
+            // max = max < cons.size() ? cons.size() : max;
+            for (int i = 0; i < max; i++) {
+                System.out.print(companyName);
+                System.out.print("|" + positions.get(i).text());
+                System.out.print("|" + pros.get(i).text());
+                System.out.println("|" + cons.get(i).text());
+            }
         }
 
         System.out.println("Done!");
---
+++ WebCrawl/src/main/java/web/page/crawl/GECrawl.java
@@ -0,0 +1,27 @@
+package web.page.crawl;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class GECrawl {
+
+    public static void main(String[] args) throws Exception {
+        String companyName = "GE";
+        String companyReviewURL = "http://www.glassdoor.com.au/Reviews/GE-Reviews-E277.htm";
+        Document companyReviewPage = Jsoup.connect(companyReviewURL).userAgent("Mozilla").get();
+        Elements positions = companyReviewPage.select("span.authorJobTitle.cell.middle.padHorzSm");
+        Elements pros = companyReviewPage.select("p.pros.noMargVert.notranslate");
+        Elements cons = companyReviewPage.select("p.cons.noMargVert.notranslate");
+        int max = positions.size();
+        // max = max < pros.size() ? pros.size() : max;
+        // max = max < cons.size() ? cons.size() : max;
+        for (int i = 0; i < max; i++) {
+            System.out.print(companyName);
+            System.out.print("|" + positions.get(i).text());
+            System.out.print("|" + pros.get(i).text());
+            System.out.println("|" + cons.get(i).text());
+        }
+    }
+
+}
